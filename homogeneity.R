0d112c70c0480b964d642aadfc1da6bd60a3c4bc
gilcu2 <gilcu2@gmail.com>
2017-01-11 11:51:43 +0100

    edge weights

--- homogeneity.R
+++ homogeneity.R
@@ -1,12 +1,24 @@
 
-# Find comunity by looking for the edge with less homegeneity and delete it. The comunity are the conected components in the final graph
-communityByHomogeneity<-function(g,n) {
-  g1=igraph::delete_vertex_attr(g,"name")  # Remove name attr
-  nei=igraph::ego(g1,1)  #neighbors of each vertex
-  neiV=sapply(nei,igraph::as_ids) # each neighborhood as normal vector
+#keep in each node the intersection of the vicinity of its neighbors
+intersectVicinity<-function(neighV) {
   neiV1=sapply(neiV,function(v) {neiV[v]}) # for each vertex, list of neihborhood of its neighbors
-  inter=sapply(neiV1,function(v) {Reduce(intersect,v)})
+  sapply(neiV1,function(v) {Reduce(intersect,v)}) # Intersection of neighborhoods
+}
+
+# Find comunity by looking for the edge with less homegeneity and delete it. The comunity are the conected components in the final graph
+communityByHomogeneity<-function(g,nComunity=2,order=1,fVicinity=function(neighV) {neighV}) {
   
-  #neighborhood of each vertice and its neighbors
+  # Vicinity of each node
+  g1=igraph::delete_vertex_attr(g,"name")  # Remove name attr
+  nei=igraph::ego(g1,order)  #neighbors of each vertex
+  neiV=sapply(nei,igraph::as_ids) # each neighborhood as normal vector
+  neiV1=fVicinity(neiV) # preprocess vicinities
+  
+  #edges weights
+  edges=igraph::as_edgelist(g1)
+  edges_weight=apply(edges,1,function(e) {
+    length(intersect(neiV[[e[1]]],neiV[[e[2]]]))^2/(length(neiV[[e[1]]])*length(neiV[[e[2]]]))
+    })
+  
   
 }
--- toRemember.R
+++ toRemember.R
@@ -2,3 +2,6 @@
 
 # Set as label edge_betweenness
 g1=set_edge_attr(g,"label",value=edge_betweenness(g,directed=F))
+
+# example graph
+g2=graph_from_literal(a-b-c-d-e-f, a-g-h-b, h-e:f:i, j)
